e001a991c680ed46239a52b2ad0222a5e261f42b
ymsk11 <105350536+ymsk11@users.noreply.github.com>
2022-12-31 14:52:57 +0900

    run ktlintFormat

--- app/src/main/kotlin/com/github/ymsk11/sexp/Atom.kt
+++ app/src/main/kotlin/com/github/ymsk11/sexp/Atom.kt
@@ -1,6 +1,6 @@
 package com.github.ymsk11.sexp
 
 @JvmInline
-value class Atom(val value: String): Sexp {
+value class Atom(val value: String) : Sexp {
     override fun toString(): String = value
 }
