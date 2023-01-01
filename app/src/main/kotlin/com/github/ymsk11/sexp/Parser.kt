c04fbe7f72a4a1addea5cd1eb5154778714cbcb5
ymsk11 <105350536+ymsk11@users.noreply.github.com>
2023-01-01 19:13:12 +0900

    pass test ((1))

--- app/src/main/kotlin/com/github/ymsk11/sexp/Parser.kt
+++ app/src/main/kotlin/com/github/ymsk11/sexp/Parser.kt
@@ -11,17 +11,50 @@ class Parser(
     private fun parse(token: Token): Sexp = when (token) {
         Token.Nil -> Nil
         is Token.Symbol -> Atom(token.value)
-        else -> Nil
+        else -> Nil // TODO: throw error
     }
     private fun parse(tokens: List<Token>): Sexp {
         if (tokens.isEmpty()) return Nil
         if (tokens.size == 1) {
             return parse(tokens.first())
         }
+        println("HOGEHOGE $tokens")
 
-        val token = tokens.filterNot { it == Token.RParen || it == Token.LParen }
-        val car = token.first()
-        val cdr = token.subList(1, token.size)
+        // 左右のLParen, RParenを取り除く
+        val tokens = tokens.filterIndexed { index, token ->
+            ((index == 0 && token == Token.LParen) || (index == tokens.lastIndex && token == Token.RParen)).not()
+        }
+        println("REMOVE LRParen $tokens")
+
+        var car: List<Token> = emptyList()
+        var cdr: List<Token> = emptyList()
+        if (tokens.first() == Token.LParen) {
+            // 一番最初がLParenだったら、Car部分までを取り出す
+            var nestCount = 0
+            var carLastIndex = 0
+            println("Car is list of tokens")
+            tokens.forEachIndexed { index, token ->
+                when (token) {
+                    Token.LParen -> nestCount++
+                    Token.RParen -> {
+                        nestCount--
+                        if (nestCount == 0) {
+                            carLastIndex = index
+                            return@forEachIndexed
+                        }
+                    }
+                    else -> {}
+                }
+            }
+            car = tokens.subList(0, carLastIndex)
+            cdr = tokens.subList(carLastIndex, tokens.lastIndex)
+        } else {
+            println("Car is First of Tokens")
+            car = listOf(tokens.first())
+            cdr = tokens.subList(1, tokens.size)
+        }
+        println("CAR $car")
+        println("CDR $cdr")
 
         val cdrCell = if (cdr.isNotEmpty()) {
             when (val s = parse(cdr)) {
