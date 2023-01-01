5b3b23191a39eb713b7dd895a44f7ede5ce93c92
ymsk11 <105350536+ymsk11@users.noreply.github.com>
2023-01-01 19:18:58 +0900

    remove debug comment

--- app/src/main/kotlin/com/github/ymsk11/sexp/Parser.kt
+++ app/src/main/kotlin/com/github/ymsk11/sexp/Parser.kt
@@ -18,21 +18,18 @@ class Parser(
         if (tokens.size == 1) {
             return parse(tokens.first())
         }
-        println("HOGEHOGE $tokens")
 
         // 左右のLParen, RParenを取り除く
         val tokens = tokens.filterIndexed { index, token ->
             ((index == 0 && token == Token.LParen) || (index == tokens.lastIndex && token == Token.RParen)).not()
         }
-        println("REMOVE LRParen $tokens")
 
-        var car: List<Token> = emptyList()
-        var cdr: List<Token> = emptyList()
+        var car: List<Token>
+        var cdr: List<Token>
         if (tokens.first() == Token.LParen) {
             // 一番最初がLParenだったら、Car部分までを取り出す
             var nestCount = 0
             var carLastIndex = 0
-            println("Car is list of tokens")
             tokens.forEachIndexed { index, token ->
                 when (token) {
                     Token.LParen -> nestCount++
@@ -49,12 +46,9 @@ class Parser(
             car = tokens.subList(0, carLastIndex)
             cdr = tokens.subList(carLastIndex, tokens.lastIndex)
         } else {
-            println("Car is First of Tokens")
             car = listOf(tokens.first())
             cdr = tokens.subList(1, tokens.size)
         }
-        println("CAR $car")
-        println("CDR $cdr")
 
         val cdrCell = if (cdr.isNotEmpty()) {
             when (val s = parse(cdr)) {
