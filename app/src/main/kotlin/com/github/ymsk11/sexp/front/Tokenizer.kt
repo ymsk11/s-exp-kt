731d9ffee6f9fce385cd93d0f95e6a05edcd6d31
ymsk11 <105350536+ymsk11@users.noreply.github.com>
2023-01-05 21:09:18 +0900

    refactor Tokenizer

--- app/src/main/kotlin/com/github/ymsk11/sexp/front/Tokenizer.kt
+++ app/src/main/kotlin/com/github/ymsk11/sexp/front/Tokenizer.kt
@@ -1,11 +1,45 @@
 package com.github.ymsk11.sexp.front
 
 class Tokenizer {
-    operator fun invoke(text: String): List<Token> = text
-        .replace("(", " ( ")
-        .replace(")", " ) ")
-        .replace(".", " . ")
-        .split(" ")
-        .filter { it.isNotEmpty() }
-        .map { Token.from(it) }
+
+    operator fun invoke(text: String): List<Token> {
+        val list = mutableListOf<Token>()
+        var index = 0
+        while (index <= text.lastIndex) {
+            val target = text.substring(index)
+            when {
+                target[0].isWhitespace() -> {
+                    index++
+                }
+                target[0] == '(' -> {
+                    list += Token.LParen
+                    index++
+                }
+                target[0] == ')' -> {
+                    list += Token.RParen
+                    index++
+                }
+                target[0] == '.' -> {
+                    list += Token.Dot
+                    index++
+                }
+                else -> {
+                    val last = target.indexOfFirst {
+                        it.isWhitespace() ||
+                            it == '(' ||
+                            it == ')' ||
+                            it == '.'
+                    }
+                    val value = if (last < 0) {
+                        target.substring(0)
+                    } else {
+                        target.substring(0, last)
+                    }
+                    list += Token.from(value)
+                    index += value.length
+                }
+            }
+        }
+        return list
+    }
 }
