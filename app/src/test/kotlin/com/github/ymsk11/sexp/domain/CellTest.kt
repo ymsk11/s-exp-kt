fff947749ab9d7aa50e2416cf560ad6c22360a43
ymsk11 <105350536+ymsk11@users.noreply.github.com>
2023-01-09 17:01:27 +0900

    use assertThat

--- app/src/test/kotlin/com/github/ymsk11/sexp/domain/CellTest.kt
+++ app/src/test/kotlin/com/github/ymsk11/sexp/domain/CellTest.kt
@@ -1,11 +1,16 @@
 package com.github.ymsk11.sexp.domain
 
+import com.google.common.truth.Truth.assertThat
 import org.junit.jupiter.api.Test
 
 class CellTest {
     @Test
     fun cellToString() {
-        val cell = Cell(Atom.Symbol("1"), Cell(Atom.Symbol("2"), Atom.Symbol("3")))
-        assert(cell.toString() == "( 1 . ( 2 . 3 ) )")
+        val testCases = mapOf(
+            Cell(Atom.Symbol("1"), Cell(Atom.Symbol("2"), Atom.Symbol("3"))) to "( 1 . ( 2 . 3 ) )"
+        )
+        testCases.forEach { (input, expect) ->
+            assertThat(input.toString()).isEqualTo(expect)
+        }
     }
 }
