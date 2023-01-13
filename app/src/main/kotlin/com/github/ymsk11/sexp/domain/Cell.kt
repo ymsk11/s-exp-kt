d7784dc3733e46fb4eaccda45050b3fd3022a862
ymsk11 <105350536+ymsk11@users.noreply.github.com>
2023-01-13 19:39:07 +0900

    add CellIterator

--- app/src/main/kotlin/com/github/ymsk11/sexp/domain/Cell.kt
+++ app/src/main/kotlin/com/github/ymsk11/sexp/domain/Cell.kt
@@ -3,6 +3,27 @@ package com.github.ymsk11.sexp.domain
 data class Cell(
     val car: Sexp,
     val cdr: Sexp,
-) : Sexp {
+) : Sexp, Iterable<Sexp> {
+    override fun iterator(): Iterator<Sexp> = CellIterator(this)
+
     override fun toString(): String = "( $car . $cdr )"
+}
+
+private class CellIterator(
+    private var cell: Sexp
+) : Iterator<Sexp> {
+    override fun hasNext(): Boolean {
+        if (cell is Atom) return false // cdr 部分が Atom.Nil以外でもhasNextはfalseとする
+        return true
+    }
+
+    override fun next(): Sexp {
+        return when (val target = cell) {
+            is Atom -> target
+            is Cell -> {
+                cell = target.cdr
+                target.car
+            }
+        }
+    }
 }
--- app/src/test/kotlin/com/github/ymsk11/sexp/domain/CellTest.kt
+++ app/src/test/kotlin/com/github/ymsk11/sexp/domain/CellTest.kt
@@ -13,4 +13,19 @@ class CellTest {
             assertThat(input.toString()).isEqualTo(expect)
         }
     }
+
+    @Test
+    fun cellIterator() {
+        val testCases = mapOf(
+            Cell(Atom.Symbol("1"), Cell(Atom.Symbol("2"), Cell(Atom.Symbol("3"), Atom.Nil)))
+                to listOf(Atom.Symbol("1"), Atom.Symbol("2"), Atom.Symbol("3")),
+            Cell(Atom.Symbol("1"), Atom.Symbol("2"))
+                to listOf(Atom.Symbol("1")),
+            Cell(Cell(Atom.Symbol("1"), Atom.Symbol("2")), Cell(Atom.Symbol("3"), Atom.Nil))
+                to listOf(Cell(Atom.Symbol("1"), Atom.Symbol("2")), Atom.Symbol("3"))
+        )
+        testCases.forEach { (input, expect) ->
+            assertThat(input.toList()).isEqualTo(expect)
+        }
+    }
 }
