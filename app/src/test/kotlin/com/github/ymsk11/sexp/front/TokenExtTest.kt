0a28f44489cdac7f255a00f40224ab7854bdbcb1
ymsk11 <105350536+ymsk11@users.noreply.github.com>
2023-01-17 22:38:32 +0900

    add fail testcases

--- app/src/test/kotlin/com/github/ymsk11/sexp/front/TokenExtTest.kt
+++ app/src/test/kotlin/com/github/ymsk11/sexp/front/TokenExtTest.kt
@@ -22,7 +22,15 @@ class TokenExtTest {
             listOf(Token.LParen, Token.RParen, Token.LParen, Token.RParen)
                 to listOf(listOf(Token.LParen, Token.RParen), listOf(Token.LParen, Token.RParen)),
             listOf(Token.LParen, Token.LParen, Token.RParen, Token.RParen, Token.LParen, Token.RParen)
-                to listOf(listOf(Token.LParen, Token.LParen, Token.RParen, Token.RParen), listOf(Token.LParen, Token.RParen))
+                to listOf(listOf(Token.LParen, Token.LParen, Token.RParen, Token.RParen), listOf(Token.LParen, Token.RParen)),
+            listOf(Token.Nil, Token.Nil) to listOf(listOf(Token.Nil), listOf(Token.Nil)),
+            listOf(Token.LParen, Token.RParen, Token.Nil, Token.LParen, Token.Nil, Token.RParen, Token.Nil)
+                to listOf(
+                    listOf(Token.LParen, Token.RParen),
+                    listOf(Token.Nil),
+                    listOf(Token.LParen, Token.Nil, Token.RParen),
+                    listOf(Token.Nil)
+                )
         )
         testCases.forEach { (input, expect) ->
             assertThat(input.splitMultipleParen()).isEqualTo(expect)
