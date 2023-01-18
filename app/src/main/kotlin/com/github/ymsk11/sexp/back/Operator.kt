9e92e22fb835372854627eb18e04ebbc1169535d
ymsk11 <105350536+ymsk11@users.noreply.github.com>
2023-01-18 23:02:40 +0900

    remove Fold class

--- app/src/main/kotlin/com/github/ymsk11/sexp/back/Operator.kt
+++ app/src/main/kotlin/com/github/ymsk11/sexp/back/Operator.kt
@@ -9,66 +9,41 @@ interface Operator {
     fun eval(args: Sexp, setEnvionment: (Atom.Symbol, Sexp) -> Unit, eval: (Sexp) -> Sexp): Sexp
 }
 
-class Fold(
-    private val accum: Atom,
-    private val fn: (Atom, Atom) -> Atom
-) : Operator {
-    override val symbol: String = "fold"
-    override fun eval(args: Sexp, setEnvionment: (Atom.Symbol, Sexp) -> Unit, eval: (Sexp) -> Sexp): Atom {
-        println("$accum $args")
-        if (args is Atom.Nil) return accum
-        if (args is Cell) {
-            val next = fn(accum, eval(args.car) as Atom)
-            return Fold(next, fn).eval(args.cdr, setEnvionment, eval)
-        }
-        throw IllegalArgumentException("foldに失敗しました")
-    }
-}
-
 class Addition() : Operator {
     override val symbol: String = "+"
 
-    private val fn = { a: Atom, b: Atom ->
-        if (a is Atom.IntNumber && b is Atom.IntNumber) {
-            Atom.IntNumber(a.value + b.value)
-        } else {
-            throw IllegalArgumentException("引数が整数ではありません")
-        }
-    }
-
     override fun eval(args: Sexp, setEnvionment: (Atom.Symbol, Sexp) -> Unit, eval: (Sexp) -> Sexp): Sexp {
-        return Fold(Atom.IntNumber(0), fn).eval(args, setEnvionment, eval)
+        if (args is Cell) {
+            return args.fold(Atom.IntNumber(0)) { acc, sexp ->
+                Atom.IntNumber(acc.value + (eval(sexp) as Atom.IntNumber).value)
+            }
+        }
+        throw IllegalArgumentException("引数が整数ではありません")
     }
 }
 
 class Multiplication() : Operator {
     override val symbol: String = "*"
 
-    private val fn = { a: Atom, b: Atom ->
-        if (a is Atom.IntNumber && b is Atom.IntNumber) {
-            Atom.IntNumber(a.value * b.value)
-        } else {
-            throw IllegalArgumentException("error")
-        }
-    }
-
     override fun eval(args: Sexp, setEnvionment: (Atom.Symbol, Sexp) -> Unit, eval: (Sexp) -> Sexp): Sexp {
-        return Fold(Atom.IntNumber(1), fn).eval(args, setEnvionment, eval)
+        if (args is Cell) {
+            return args.fold(Atom.IntNumber(1)) { acc, sexp ->
+                Atom.IntNumber(acc.value * (eval(sexp) as Atom.IntNumber).value)
+            }
+        }
+        throw IllegalArgumentException("引数が整数ではありません")
     }
 }
 
 class Subtraction() : Operator {
     override val symbol: String = "-"
 
-    private val fn = { a: Atom, b: Atom ->
-        if (a is Atom.IntNumber && b is Atom.IntNumber) {
-            Atom.IntNumber(a.value - b.value)
-        } else {
-            throw IllegalArgumentException("error")
-        }
-    }
     override fun eval(args: Sexp, setEnvionment: (Atom.Symbol, Sexp) -> Unit, eval: (Sexp) -> Sexp): Sexp {
-        val accum = (args as Cell).car as Atom.IntNumber
-        return Fold(accum, fn).eval(args.cdr, setEnvionment, eval)
+        if (args is Cell && args.car is Atom.IntNumber && args.cdr is Cell) {
+            return args.cdr.fold(args.car) { acc, sexp ->
+                Atom.IntNumber(acc.value - (eval(sexp) as Atom.IntNumber).value)
+            }
+        }
+        throw IllegalArgumentException("引数が整数ではありません")
     }
 }
